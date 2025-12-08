fb4dd22fe0cf3aad470dfb2d3a7291dfb3c70c08
Demo <princekumar09372@gmail.com>
2025-12-08 12:08:26 +0530

    feat: Added Contact form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from './components/About';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <About />
       <Projects />
+      <Contact />
     </div>
   );
 }
