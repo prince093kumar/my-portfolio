c5a0f26487d947db01710193da0cc32e76e24054
Demo <princekumar09372@gmail.com>
2025-12-10 16:26:53 +0530

    Fix deploy workflow and Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/my-portfolio/",
-  plugins: [react(),tailwindcss(),
-],
-})
+  base: "/my-portfolio/",   // your GitHub repo name EXACTLY
+  plugins: [react(), tailwindcss()],
+});
